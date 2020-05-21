823349e24a6545f2d81de1f7b39adfa3dced2736
matthew <devnull@localhost>
2020-05-21 19:15:09 +0100

    Don't count SWIs twice for the instruction count

--- src/arm_common.c
+++ src/arm_common.c
@@ -570,8 +570,6 @@ opSWI(uint32_t opcode)
 {
 	uint32_t swinum = opcode & 0xdffff;
 
-	inscount++;
-
 	/* Get actual SWI number from OS_CallASWI and OS_CallASWIR12 */
 	if (swinum == SWI_OS_CallASWI) {
 		swinum = arm.reg[10] & 0xdffff;
