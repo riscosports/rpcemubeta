3a8bff50a72a0483f47008fac58cb496c3554c0f
matthew <devnull@localhost>
2021-09-03 21:13:37 +0100

    Simplify code for ADC reg in 32-bit dynarec

--- src/codegen_x86.c
+++ src/codegen_x86.c
@@ -1210,14 +1210,11 @@ recompile(uint32_t opcode, uint32_t *pcpsr)
 		if (!generate_shift(opcode)) {
 			return 0;
 		}
-		// Shifted val now in %eax
-		addbyte(0xf6); addbyte(0x05); addptr(((char *) pcpsr) + 3); addbyte(0x20); // TESTB $0x20,pcpsr+3
-		addbyte(0x89); addbyte(0xc2); // MOV %eax,%edx
-		gen_load_reg(RN, EAX);
-		addbyte(0x74); addbyte(1); // JZ +1
-		addbyte(0x42); // INC %edx
-		addbyte(0x01); addbyte(0xd0); // ADD %edx,%eax
-		gen_save_reg(RD, EAX);
+		addbyte(0x8b); addbyte(0x0d); addptr(pcpsr); // MOV pcpsr,%ecx
+		gen_load_reg(RN, EDX);
+		addbyte(0xc1); addbyte(0xe1); addbyte(3); // SHL $3,%ecx - put ARM carry into x86 carry
+		addbyte(0x11); addbyte(0xc2); // ADC %eax,%edx
+		gen_save_reg(RD, EDX);
 		break;
 
 	case 0x0b: // ADCS reg
